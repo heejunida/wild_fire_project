996fb738778977444d06d521acaea8382424f9a8
[Heejun Chang] <[qazwsx05723@gmail.com]>
2025-07-15 09:33:08 +0900

    1. 머신러닝 데이터 전처리

--- wild_fire_client/delete.py
+++ wild_fire_client/delete.py
@@ -1,8 +1,3 @@
 import pandas as pd
-
-df = pd.read_csv("preprocess/csv/datetime_changed_end.csv")
-
-df = df.drop(columns="forest_type_mode_5x5")
-
-df.to_csv("preprocess/csv/forest_type_deleted.csv", index = False)
-df.to_excel("preprocess/csv/forest_type_deleted.xlsx", index = False)
+df = pd.read_csv('final_data.csv', dtype=str)
+print(df.columns.tolist())
--- wild_fire_client/util/to_excel.py
+++ wild_fire_client/util/to_excel.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # CSV 파일 읽기
-df = pd.read_csv("fire_weather_timeslot_FIXED_with_datetime_cols_datetimeonly.csv", encoding='utf-8-sig')
+df = pd.read_csv("preprocess/csv/ml_ready_data.csv", encoding='utf-8-sig')
 
 # 엑셀 파일로 저장
-df.to_excel("fire_weather_timeslot_FIXED_with_datetime_cols_datetimeonly.xlsx", index=False)
+df.to_excel("preprocess/csv/ml_ready_data.xlsx", index=False)
 
-print("엑셀 파일로 변환 완료: fire_weather_timeslot_FIXED_with_datetime_cols_datetimeonly.xlsx")
+print("엑셀 파일로 변환 완료: final_data_features.xlsx")
